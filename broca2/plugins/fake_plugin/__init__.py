95f2ccc06b8685e562599a0d8e8d42bbd3213a19
actuallyrizzn <guesswho@rizzn.com>
2025-08-04 13:03:46 -0500

    Preparing for the Auto-Discover refactor.

---
+++ broca2/plugins/fake_plugin/__init__.py
@@ -0,0 +1,10 @@
+"""
+Fake Plugin for testing plugin discovery
+
+This is a minimal plugin that implements the Plugin interface
+to test if Broca2 can discover and load plugins automatically.
+"""
+
+from .plugin import FakePlugin
+
+__all__ = ['FakePlugin'] 
